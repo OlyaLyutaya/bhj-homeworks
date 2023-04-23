4f6e4fd318574de48fb175985ae7752d135fb41c
olyalyutaya <you@example.com>
2023-04-23 11:29:03 +0300

    готово

---
+++ client-state/auth/task.js
@@ -0,0 +1,74 @@
+const form = document.getElementById('signin__form');
+const welcomeElement = document.getElementById('welcome');
+const loginPageElement = document.querySelector('.login__page');
+const signinElement = document.getElementById('signin');
+const logoutBtn = document.getElementById('logout__btn');
+
+// Events
+document.addEventListener('DOMContentLoaded', () => {
+  const userID = localStorage.getItem('userID');
+  if (!userID) {
+    return;
+  }
+  addWelcomeBlock(userID);
+});
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  try {
+    const response = await requestAuth();
+    requestHandler(response);
+  } catch (err) {
+    console.log(`Error send request: ${err}`);
+  }
+});
+
+logoutBtn.addEventListener('click', () => {
+  logout();
+});
+
+// Handlers
+function logout() {
+  welcomeElement.classList.remove('welcome_active');
+  loginPageElement.classList.remove('login__page-active');
+  signinElement.classList.add('signin_active');
+  localStorage.removeItem('userID');
+}
+
+function hideSigninBlock() {
+  signinElement.classList.remove('signin_active');
+}
+
+function requestHandler(data) {
+  const { success, user_id } = data;
+  if (!success) {
+    alert('Неверный логин/пароль');
+    return;
+  }
+  localStorage.setItem('userID', user_id);
+  addWelcomeBlock(user_id);
+}
+
+function addWelcomeBlock(id) {
+  hideSigninBlock();
+  welcomeElement.firstElementChild.textContent = id;
+  welcomeElement.classList.add('welcome_active');
+  loginPageElement.classList.add('login__page-active');
+}
+
+// Request
+async function requestAuth() {
+  try {
+    let response = await fetch(
+      'https://netology-slow-rest.herokuapp.com/auth.php',
+      {
+        method: 'POST',
+        body: new FormData(form),
+      },
+    );
+    response = await response.json();
+    form.reset();
+    return response;
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
