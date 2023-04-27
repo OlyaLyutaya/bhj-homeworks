8aec1c05d624609f4954cecfa5eb11d5b50f8a2e
olyalyutaya <you@example.com>
2023-04-27 09:03:05 +0300

    переделала

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -1,20 +1,17 @@
 const textArea = document.querySelector('#editor');
 const savedText = localStorage.getItem('text');
 
-const updateStorage = () => {
-  textArea.addEventListener('input', () => {
-    localStorage.setItem('text', textArea.value);
-  });
+const saveDataInLocalStorage = () => {
+  localStorage.setItem('text', textArea.value);
 };
+
+textArea.addEventListener('input', saveDataInLocalStorage);
 
 if (savedText) {
   textArea.value = savedText;
-  updateStorage();
-} else {
-  updateStorage();
-}
+} 
 
 function clearText() {
-  textareaElement.value = '';
+  textArea.value = '';
   saveDataInLocalStorage();
 }
